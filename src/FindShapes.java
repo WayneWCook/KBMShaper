24c61f24ed5eddf9a705582ed1b8295856b61e2f
mlove <mlove685160@gmail.com>
2020-04-15 15:05:26 -0600

    Added back the get area for trapezoid

--- src/FindShapes.java
+++ src/FindShapes.java
@@ -14,10 +14,12 @@ public class FindShapes {
         Rhombus rhombus = new Rhombus(5, 0.5);
         RegularPolygon hexagon = new RegularPolygon(7, 6);
         RegularPolygon pentagon = new RegularPolygon(7,5);
+        Trapezoid trapezoid = new Trapezoid(5,7,3);
         System.out.println("Rectangle are is: " + rec1.getArea());
         System.out.println("Square area is: "+ square.getArea());
         System.out.println("Pentagon area is: " + pentagon.getArea());
         System.out.println("Hexagon area is: " + hexagon.getArea()) ;
+        System.out.println("Trapezoid are is: " + trapezoid.getArea());
         System.out.println("Hexagon perimeter is: " + hexagon.getPerimeter());
         System.out.println("Pentagon perimeter is: " + pentagon.getPerimeter());
         System.out.println("The sum can go in either direction!");
