8962cb526b379fd4c7a2219d5bdb3bfa5c2705fb
WayneWCook <38863521+WayneWCook@users.noreply.github.com>
2020-04-17 15:30:20 -0600

    update Shape and FinalShapes.java so that instance are destroyed before the program exits

--- src/FindShapes.java
+++ src/FindShapes.java
@@ -1,20 +1,29 @@
 /* FindShapes.java
  * Wayne Cook
  * 25 March 2020
- * Create the main class to test the rectangle, swuare, circle and shombus classes.
- * Force a commit
+ *      Create the main class to test the rectangle, swuare, circle and shombus classes.
+ *      Force a commit
+ *  16 April 2020
+ *      Add way to track the number of items that are open and then destroyed.
  */
 //package Shape-RCC;
 
 public class FindShapes {
     public static void main(String[] args) {
         Rectangle rec1 = new Rectangle(15,10);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         Square square = new Square(7);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         Circle circle = new Circle(3);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         Rhombus rhombus = new Rhombus(5, 0.5);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         RegularPolygon hexagon = new RegularPolygon(7, 6);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         RegularPolygon pentagon = new RegularPolygon(7,5);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         Trapezoid trapezoid = new Trapezoid(5,7,3);
+        System.out.println("There are now " + rec1.getcount() + " shapes");
         System.out.println("Rectangle are is: " + rec1.getArea());
         System.out.println("Square area is: "+ square.getArea());
         System.out.println("Pentagon area is: " + pentagon.getArea());
@@ -34,7 +43,38 @@ public class FindShapes {
         System.out.println("The Pentagon and Circle areas add to: " + pentagon.addArea(circle));
         System.out.println("The Hexagon and Circle perimeters add to: " + hexagon.addPerimeter(circle));
         System.out.println("The Pentagon and rectangle perimeters add to: " + pentagon.addPerimeter(rec1));
+        // Now start the process of removing the objects from heap memory.
+        System.out.println(" The end has come. There are now " + hexagon.getcount() + " shape<");
+        square.decCount();
+        square = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        circle.decCount();
+        circle = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        rec1.decCount();
+        rec1 = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        pentagon.decCount();
+        square = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        rhombus.decCount();
+        rhombus = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        trapezoid.decCount();
+        trapezoid = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        pentagon.decCount();
+        pentagon = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        System.out.println("There are now " + hexagon.getcount() + " shapes");
+        hexagon.decCount();
+        hexagon = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        try {
+            System.out.println("There are now " + hexagon.getcount() + " shape<");
+            hexagon.decCount();
+            hexagon = null;                          // Allow  JAVA to garbarge collect detroyed instance.
+        } catch (Exception e) {
+            System.out.println("Second destruction of hexagon did not work. Already destroyed.");
+        }
     }
-
 }
 
--- src/Shape.java
+++ src/Shape.java
@@ -3,13 +3,23 @@
  * 25 March 2020
  * This is an abstract class that forces all children to define the getArea() and getPerimeter) methods
  * It also suplies the addArea() and addPerimeter() methods that can be used by any child.
+ * 16 April 2020 - Wayne Cook
+ *      Modified this class to track the number of objects that are in use that are subclasses of Shape.
  */
 //package Shape-RCC;
 
 public abstract class Shape {
+    // Attributes - Put any needed variables here and there with any default value.
+    static int count = 0;
+    // Constructors and destruct - for creating and destroying an instance of the class, if needed.
+    // None needed for an abstract class
+    Shape() {count++;}
+    // There is no real destructor in Java. .finalize() is called at some point, but not necessarily immediately.
+
     // Force children to implement the following methods.
     abstract double getArea();
     abstract double getPerimeter();
+
     //Provide the following methods for each child.
     double addArea(Shape other) {
         return (this.getArea() + other.getArea());
@@ -17,4 +27,7 @@ public abstract class Shape {
     double addPerimeter(Shape other) {
         return (this.getPerimeter() + other.getPerimeter());
     }
+    int getcount() { return count;}
+    // Allow the count to be decremated before destroying th object.
+    void decCount() {count--;}
 }
